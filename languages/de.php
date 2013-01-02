6b6aaae5e558950350338b8b84c1e70ea54849df
cmb69 <cmbecker69@gmx.de>
2013-01-02 14:07:08 +0000

    added German localization

---
+++ languages/de.php
@@ -0,0 +1,17 @@
+<?php
+
+$plugin_tx['minicounter']['html']="<p id=\"minicounter\">Sie sind der %d. Besucher seit dem 7. April 2012.</p>";
+$plugin_tx['minicounter']['html_admin']="<strong>Minicounter hat bereits %d Besucher gezählt!</strong>";
+
+$plugin_tx['minicounter']['syscheck_title']="System-Prüfung";
+$plugin_tx['minicounter']['syscheck_phpversion']="PHP-Version &ge; %s";
+$plugin_tx['minicounter']['syscheck_extension']="Erweiterung '%s' geladen";
+$plugin_tx['minicounter']['syscheck_encoding']="'UTF-8' Kodierung eingestellt";
+$plugin_tx['minicounter']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['minicounter']['syscheck_writable']="Ordner '%s' beschreibbar";
+
+$plugin_tx['minicounter']['cf_folder_data']="Pfad eines Ordners relativ zum CMSimple-Installationverzeichnis, wo die Plugindaten gespeichert werden sollen. Z.B. \"userfiles/minicounter/\". Leer lassen, um in den data/ Ordner des Plugins zu speichern.";
+$plugin_tx['minicounter']['cf_start_value']="Der Startwert für die Besucherzählung. Setzen Sie diesen größer als 0, wenn Sie zuvor einen anderen Besucherzähler installiert hatten, oder wenn Sie die Zählerdatei leeren möchten.";
+$plugin_tx['minicounter']['cf_ignore_ips']="Eine durch Kommas getrennte Liste von zu ignorierenden IP-Adressen.";
+
+?>
