25e92620171ee612827301ceeeb6f10e2b82c49d
cmb69 <cmbecker69@gmx.de>
2013-01-02 19:47:35 +0000

    added Slovak and Danish localizations (contributed by Tata resp. maeg)

---
+++ languages/da.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Copyright (c) 2012 Jens Maegaard - JmNet.dk - CMSimple-xh.dk
+ *
+ * This work is licensed under a Creative Commons Attribution 3.0 Unported
+ * License.
+ */
+
+$plugin_tx['minicounter']['html']="<p id=\"minicounter\"> Du er besøgende nr. %   </ p>";
+$plugin_tx['minicounter']['html_admin']="<strong> Minicounter tæller% d besøgende indtil nu! </ strong>";
+$plugin_tx['minicounter']['syscheck_title']="System check";
+$plugin_tx['minicounter']['syscheck_phpversion']="PHP version &ge; %s";
+$plugin_tx['minicounter']['syscheck_extension']="Extension '%s' loaded";
+$plugin_tx['minicounter']['syscheck_encoding']="Tegnkoding 'UTF-8' konfigureret";
+$plugin_tx['minicounter']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['minicounter']['syscheck_writable']="Mappen '%s' skrivebar";
+$plugin_tx['minicounter']['cf_folder_data']="Stien til en mappe i forhold til CMSimple rodmappen, hvor du vil gemme plugin data. F.eks \"userfiles / minicounter /\". Hvis feltet er tomt gemmes data i plugin data / mappe.";
+
+?>
---
+++ languages/sk.php
@@ -0,0 +1,13 @@
+<?php
+
+$plugin_tx['minicounter']['html']="<p id=\"minicounter\">Ste %d. návštevník od 01.01.2012.</p>";
+$plugin_tx['minicounter']['html_admin']="<strong>Minicounter už zaregistroval %d návštevníkov!</strong>";
+$plugin_tx['minicounter']['syscheck_title']="Kontrola systému";
+$plugin_tx['minicounter']['syscheck_phpversion']="PHP verzia &ge; %s";
+$plugin_tx['minicounter']['syscheck_extension']="Rozšírenia '%s' načítané";
+$plugin_tx['minicounter']['syscheck_encoding']="Kódovanie 'UTF-8' nastavené";
+$plugin_tx['minicounter']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['minicounter']['syscheck_writable']="Do adresára '%s' sa dá zapisovať";
+$plugin_tx['minicounter']['cf_folder_data']="Cesta k adresáru relatívna ku koreňovému adresáru CMSimple, kam sa majú ukladať dáta pluginu. Napr.: \"userfiles/minicounter/\". Ak nič neuvediete, budú sa dáta zapisovať do adresára pluginu /data.";
+
+?>
