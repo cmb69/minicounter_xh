80646238865c43ec6e76475855e7d61a4772d063
cmb69 <cmbecker69@gmx.de>
2014-01-05 14:24:02 +0000

    fixed CS violations

--- admin.php
+++ admin.php
@@ -3,87 +3,112 @@
 /**
  * Back-End of Minicounter_XH.
  *
- * @package	Minicounter
- * @copyright	Copyright (c) 2012-2013 Christoph M. Becker <http://3-magi.net/>
- * @license	http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
- * @version     $Id$
- * @link	http://3-magi.net/?CMSimple_XH/Minicounter_XH
+ * PHP version 5
+ *
+ * @category  CMSimple_XH
+ * @package   Minicounter
+ * @author    Christoph M. Becker <cmbecker69@gmx.de>
+ * @copyright 2012-2014 Christoph M. Becker <http://3-magi.net/>
+ * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @version   SVN: $Id$
+ * @link      http://3-magi.net/?CMSimple_XH/Minicounter_XH
  */
 
-
+/*
+ * Prevent direct access.
+ */
 if (!defined('CMSIMPLE_XH_VERSION')) {
     header('HTTP/1.0 403 Forbidden');
     exit;
 }
 
-
 /**
  * Returns the plugin version information view.
  *
- * @return string  The (X)HTML.
+ * @return string (X)HTML.
+ *
+ * @global array The paths of system files and folders.
+ * @global array The localization of the plugins.
+ *
+ * @todo Fix TAGCs.
  */
 function Minicounter_version()
 {
     global $pth, $plugin_tx;
 
-    return '<h1><a href="http://3-magi.net/?CMSimple_XH/Minicounter_XH">Minicounter_XH</a></h1>'
-	. tag('img src="' . $pth['folder']['plugins'] . 'minicounter/minicounter.png" alt="Plugin Icon" style="float: left; margin: 0"')
-	. '<p>Version: ' . MINICOUNTER_VERSION . '</p>'
-	. '<p>Copyright &copy; 2012 <a href="http://3-magi.net">Christoph M. Becker</a></p>'
-	. '<p>' . sprintf($plugin_tx['minicounter']['html_admin'], Minicounter_count()) . '</p>'
-	. '<p style="text-align: justify">This program is free software: you can redistribute it and/or modify'
-	. ' it under the terms of the GNU General Public License as published by'
-	. ' the Free Software Foundation, either version 3 of the License, or'
-	. ' (at your option) any later version.</p>'
-	. '<p style="text-align: justify">This program is distributed in the hope that it will be useful,'
-	. ' but WITHOUT ANY WARRANTY; without even the implied warranty of'
-	. ' MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the'
-	. ' GNU General Public License for more details.</p>'
-	. '<p style="text-align: justify">You should have received a copy of the GNU General Public License'
-	. ' along with this program.  If not, see'
-	. ' <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>';
+    $version = MINICOUNTER_VERSION;
+    $count = sprintf($plugin_tx['minicounter']['html_admin'], Minicounter_count());
+    return <<<EOT
+<h1><a href="http://3-magi.net/?CMSimple_XH/Minicounter_XH">Minicounter_XH</a></h1>
+<img src="{$pth['folder']['plugins']}minicounter/minicounter.png"
+     alt="Plugin Icon" style="float: left; margin: 0" />
+<p>Version: $version</p>
+<p>Copyright &copy; 2012-2014 <a href="http://3-magi.net">Christoph M. Becker</a></p>
+<p>$count</p>
+<p style="text-align: justify">
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+</p>
+<p style="text-align: justify">
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+</p>
+<p style="text-align: justify">
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see
+    <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
+</p>
+EOT;
 }
-
 
 /**
  * Returns the requirements information view.
  *
- * @return string  The (X)HTML.
+ * @return string (X)HTML.
+ *
+ * @global array The paths of system files and folders.
+ * @global array The localization of the core.
+ * @global array The localization of the plugins.
  */
-function Minicounter_systemCheck() // RELEASE-TODO
+function Minicounter_systemCheck()
 {
     global $pth, $tx, $plugin_tx;
 
-    define('MINICOUNTER_PHP_VERSION', '4.0.7');
+    $phpVersion = '4.0.7';
     $ptx = $plugin_tx['minicounter'];
     $imgdir = $pth['folder']['plugins'] . 'minicounter/images/';
     $ok = tag('img src="' . $imgdir . 'ok.png" alt="ok"');
     $warn = tag('img src="' . $imgdir . 'warn.png" alt="warning"');
     $fail = tag('img src="' . $imgdir . 'fail.png" alt="failure"');
     $o = '<h4>' . $ptx['syscheck_title'] . '</h4>'
-	. (version_compare(PHP_VERSION, MINICOUNTER_PHP_VERSION) >= 0 ? $ok : $fail)
-	. '&nbsp;&nbsp;' . sprintf($ptx['syscheck_phpversion'], MINICOUNTER_PHP_VERSION)
-	. tag('br');
+        . (version_compare(PHP_VERSION, $phpVersion) >= 0 ? $ok : $fail)
+        . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_phpversion'], $phpVersion)
+        . tag('br');
     foreach (array('session') as $ext) {
-	$o .= (extension_loaded($ext) ? $ok : $fail)
-	    . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_extension'], $ext) . tag('br');
+        $o .= (extension_loaded($ext) ? $ok : $fail)
+            . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_extension'], $ext)
+            . tag('br');
     }
     $o .= (!get_magic_quotes_runtime() ? $ok : $fail)
-	. '&nbsp;&nbsp;' . $ptx['syscheck_magic_quotes'] . tag('br') . tag('br');
+        . '&nbsp;&nbsp;' . $ptx['syscheck_magic_quotes'] . tag('br') . tag('br');
     $o .= (strtoupper($tx['meta']['codepage']) == 'UTF-8' ? $ok : $warn)
-	. '&nbsp;&nbsp;' . $ptx['syscheck_encoding'] . tag('br') . tag('br');
+        . '&nbsp;&nbsp;' . $ptx['syscheck_encoding'] . tag('br') . tag('br');
     $folders = array();
     foreach (array('config/', 'languages/') as $folder) {
-	$folders[] = $pth['folder']['plugins'] . 'minicounter/' . $folder;
+        $folders[] = $pth['folder']['plugins'] . 'minicounter/' . $folder;
     }
     $folders[] = Minicounter_dataFolder();
     foreach ($folders as $folder) {
-	$o .= (is_writable($folder) ? $ok : $warn)
-	    . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_writable'], $folder) . tag('br');
+        $o .= (is_writable($folder) ? $ok : $warn)
+            . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_writable'], $folder)
+            . tag('br');
     }
     return $o;
 }
-
 
 /*
  * Handle the plugin administration.
@@ -92,10 +117,10 @@ if (isset($minicounter) && $minicounter == 'true') {
     $o .= print_plugin_admin('off');
     switch ($admin) {
     case '':
-	$o .= Minicounter_version() . tag('hr') . Minicounter_systemCheck();
-	break;
+        $o .= Minicounter_version() . tag('hr') . Minicounter_systemCheck();
+        break;
     default:
-	$o .= plugin_admin_common($action, $admin, $plugin);
+        $o .= plugin_admin_common($action, $admin, $plugin);
     }
 }
 
--- index.php
+++ index.php
@@ -3,27 +3,37 @@
 /**
  * Front-end of Minicounter_XH.
  *
- * @package	Minicounter
- * @copyright	Copyright (c) 2012-2013 Christoph M. Becker <http://3-magi.net/>
- * @license	http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
- * @version     $Id$
- * @link	http://3-magi.net/?CMSimple_XH/Minicounter_XH
+ * PHP version 5
+ *
+ * @category  CMSimple_XH
+ * @package   Minicounter
+ * @author    Christoph M. Becker <cmbecker69@gmx.de>
+ * @copyright 2012-2014 Christoph M. Becker <http://3-magi.net/>
+ * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @version   SVN: $Id$
+ * @link      http://3-magi.net/?CMSimple_XH/Minicounter_XH
  */
 
-
+/*
+ * Prevent direct access.
+ */
 if (!defined('CMSIMPLE_XH_VERSION')) {
     header('HTTP/1.0 403 Forbidden');
     exit;
 }
 
-
+/**
+ * The version number.
+ */
 define('MINICOUNTER_VERSION', '1rc1');
-
 
 /**
  * Returns the data folder.
  *
  * @return string
+ *
+ * @global array The paths of system files and folders.
+ * @global array The configuration of the plugins.
  */
 function Minicounter_dataFolder()
 {
@@ -32,25 +42,24 @@ function Minicounter_dataFolder()
     $pcf = $plugin_cf['minicounter'];
 
     if ($pcf['folder_data'] == '') {
-	$fn = $pth['folder']['plugins'].'minicounter/data/';
+        $fn = $pth['folder']['plugins'] . 'minicounter/data/';
     } else {
-	$fn = $pth['folder']['base'] . $pcf['folder_data'];
+        $fn = $pth['folder']['base'] . $pcf['folder_data'];
     }
     if (substr($fn, -1) != '/') {
-	$fn .= '/';
+        $fn .= '/';
     }
     if (file_exists($fn)) {
-	if (!is_dir($fn)) {
-	    e('cntopen', 'folder', $fn);
-	}
+        if (!is_dir($fn)) {
+            e('cntopen', 'folder', $fn);
+        }
     } else {
-	if (!mkdir($fn, 0777, true)) {
-	    e('cntwriteto', 'folder', $fn);
-	}
+        if (!mkdir($fn, 0777, true)) {
+            e('cntwriteto', 'folder', $fn);
+        }
     }
     return $fn;
 }
-
 
 /**
  * Increases the visitor counter by one.
@@ -61,19 +70,21 @@ function Minicounter_increase()
 {
     $fn = Minicounter_dataFolder() . 'count.txt';
     if (($fh = fopen($fn, 'a')) === false
-	    || fwrite($fh, '*') === false) {
-	e('cntwriteto', 'file', $fn);
+        || fwrite($fh, '*') === false
+    ) {
+        e('cntwriteto', 'file', $fn);
     }
     if ($fh !== false) {
-	fclose($fh);
+        fclose($fh);
     }
 }
-
 
 /**
  * Returns the visitor count.
  *
  * @return int
+ *
+ * @global array The configuration of the plugins.
  */
 function Minicounter_count()
 {
@@ -82,16 +93,22 @@ function Minicounter_count()
     $count = $plugin_cf['minicounter']['start_value'];
     $fn = Minicounter_dataFolder() . 'count.txt';
     if (is_readable($fn)) {
-	$count += filesize($fn);
+        $count += filesize($fn);
     }
     return $count;
 }
-
 
 /**
  * Handles the visitor counting.
  *
  * @return void
+ *
+ * @global bool   Whether we're in admin mode.
+ * @global string The requested system function.
+ * @global bool   Whether logout is requested.
+ * @global array  The configuration of the plugins.
+ *
+ * @todo Adapt for XH 1.6.
  */
 function Minicounter_doCount()
 {
@@ -99,48 +116,49 @@ function Minicounter_doCount()
 
     $pcf = $plugin_cf['minicounter'];
     if ($pcf['honor_dnt']
-	&& isset($_SERVER['HTTP_DNT']) && $_SERVER['HTTP_DNT'])
-    {
-	return;
+        && isset($_SERVER['HTTP_DNT']) && $_SERVER['HTTP_DNT']
+    ) {
+        return;
     }
 
-    if (!isset($_SESSION)) {
-	session_start();
+    if (session_id() == '') {
+        session_start();
     }
     if (!isset($_SESSION['minicounter_count'][CMSIMPLE_ROOT])) {
-	$_SESSION['minicounter_count'][CMSIMPLE_ROOT] = Minicounter_count() + 1;
+        $_SESSION['minicounter_count'][CMSIMPLE_ROOT] = Minicounter_count() + 1;
     }
     $ips = explode(',', $pcf['ignore_ips']);
     $ips = array_map('trim', $ips);
-    if (!$adm && $f != 'login' && !$logout && !in_array($_SERVER['REMOTE_ADDR'], $ips)) {
-	if (!isset($_SESSION['minicounter_counted'][CMSIMPLE_ROOT])) {
-	    $_SESSION['minicounter_counted'][CMSIMPLE_ROOT] = false;
-	} else {
-	    if ($_SESSION['minicounter_counted'][CMSIMPLE_ROOT] === false) {
-		minicounter_increase();
-		$_SESSION['minicounter_counted'][CMSIMPLE_ROOT] = true;
-	    }
-	}
+    if (!$adm && $f != 'login' && !$logout
+        && !in_array($_SERVER['REMOTE_ADDR'], $ips)
+    ) {
+        if (!isset($_SESSION['minicounter_counted'][CMSIMPLE_ROOT])) {
+            $_SESSION['minicounter_counted'][CMSIMPLE_ROOT] = false;
+        } else {
+            if ($_SESSION['minicounter_counted'][CMSIMPLE_ROOT] === false) {
+                minicounter_increase();
+                $_SESSION['minicounter_counted'][CMSIMPLE_ROOT] = true;
+            }
+        }
     }
 }
-
 
 /**
  * Returns the visitor counter.
  *
- * @access public
- * @return string  The (X)HTML.
+ * @return string (X)HTML.
+ *
+ * @global array The localization of the plugins.
  */
 function minicounter()
 {
     global $plugin_tx;
 
     $count = isset($_SESSION['minicounter_count'][CMSIMPLE_ROOT])
-	? $_SESSION['minicounter_count'][CMSIMPLE_ROOT]
-	: Minicounter_count() + 1;
+        ? $_SESSION['minicounter_count'][CMSIMPLE_ROOT]
+        : Minicounter_count() + 1;
     return sprintf($plugin_tx['minicounter']['html'], $count);
 }
-
 
 /*
  * Handle the visitor counting.
