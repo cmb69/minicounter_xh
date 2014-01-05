8289c47422e470bb672f8fdbbb51e7bffc90f754
cmb69 <cmbecker69@gmx.de>
2014-01-05 14:40:26 +0000

    adjusted for build script

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if (!defined('CMSIMPLE_XH_VERSION')) {
 /**
  * The version number.
  */
-define('MINICOUNTER_VERSION', '1rc1');
+define('MINICOUNTER_VERSION', '@MINICOUNTER_VERSION@');
 
 /**
  * Returns the data folder.
