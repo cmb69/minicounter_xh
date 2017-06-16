9eeaa94bf5a03167eb6d3df3f7e643481f90fecc
Christoph M. Becker <cmbecker69@gmx.de>
2017-06-16 13:39:40 +0200

    Which program?

--- classes/Views.php
+++ classes/Views.php
@@ -81,20 +81,20 @@ class Minicounter_Views
 <p>Version: $version</p>
 <p>Copyright &copy; 2012-2017 <a href="http://3-magi.net">Christoph M. Becker</a></p>
 <p style="text-align: justify">
-    This program is free software: you can redistribute it and/or modify
+    Minicounter_XH is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 </p>
 <p style="text-align: justify">
-    This program is distributed in the hope that it will be useful,
+    Minicounter_XH is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 </p>
 <p style="text-align: justify">
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see
+    along with Minicounter_XH.  If not, see
     <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
 </p>
 EOT;
