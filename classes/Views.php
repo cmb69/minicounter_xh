0052e96119f7e5866f2d51c05ed41d1bc866f5d6
Christoph M. Becker <cmbecker69@gmx.de>
2017-06-16 16:02:42 +0200

    Extract info view

--- classes/Views.php
+++ classes/Views.php
@@ -57,32 +57,10 @@ class Views
     {
         global $plugin_tx;
 
-        $version = MINICOUNTER_VERSION;
-        $pluginIconPath = $this->model->pluginIconPath();
-        return <<<EOT
-<h4>{$plugin_tx['minicounter']['about']}</h4>
-<img src="$pluginIconPath" alt="Plugin Icon"
-     style="float: left; margin: 0; width: 128px; height: 128px" />
-<p>Version: $version</p>
-<p>Copyright &copy; 2012-2017 <a href="http://3-magi.net">Christoph M. Becker</a></p>
-<p style="text-align: justify">
-    Minicounter_XH is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-</p>
-<p style="text-align: justify">
-    Minicounter_XH is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-</p>
-<p style="text-align: justify">
-    You should have received a copy of the GNU General Public License
-    along with Minicounter_XH.  If not, see
-    <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
-</p>
-EOT;
+        $view = new View('info');
+        $view->logo = $this->model->pluginIconPath();
+        $view->version = MINICOUNTER_VERSION;
+        return (string) $view;
     }
 
     /**
