6dfb191b9c41f5e2bcc2db87ca8fe09343f63284
Christoph M. Becker <cmbecker69@gmx.de>
2017-06-17 12:23:29 +0200

    No need to prevent direct access

--- index.php
+++ index.php
@@ -1,26 +1,23 @@
 <?php
 
 /**
- * Initialization.
+ * Copyright 2012-2017 Christoph M. Becker
  *
- * PHP version 5
+ * This file is part of Minicounter_XH.
  *
- * @category  CMSimple_XH
- * @package   Minicounter
- * @author    Christoph M. Becker <cmbecker69@gmx.de>
- * @copyright 2012-2017 Christoph M. Becker <http://3-magi.net/>
- * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
- * @version   SVN: $Id$
- * @link      http://3-magi.net/?CMSimple_XH/Minicounter_XH
+ * Minicounter_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Minicounter_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Minicounter_XH.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-/*
- * Prevent direct access.
- */
-if (!defined('CMSIMPLE_XH_VERSION')) {
-    header('HTTP/1.0 403 Forbidden');
-    exit;
-}
 
 /**
  * @return string
